f497f833dee0ac393d5c93057794a64f85da277a
Asmit Kumar <69111936+Asmit108@users.noreply.github.com>
2024-08-17 17:46:59 +0530

    server url changes

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const jwt=localStorage.getItem("jwt")
-export const API_BASE_URL='http://localhost:5454'
+export const API_BASE_URL=process.env.REACT_APP_SERVER_BASE_URL
 
 export const api=axios.create({
     baseURL:API_BASE_URL,
